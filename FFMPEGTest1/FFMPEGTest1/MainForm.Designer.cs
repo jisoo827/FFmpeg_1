8303e7fb78984867ba67a3f5c5030c2966e50685
jisoo827 <73168875+jisoo827@users.noreply.github.com>
2020-10-21 17:07:55 +0900

    Add files via upload

---
+++ FFMPEGTest1/FFMPEGTest1/MainForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace FFMPEGTest1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Img_video = new System.Windows.Forms.PictureBox();
+            this.Btn_Play = new System.Windows.Forms.Button();
+            this.Btn_Stop = new System.Windows.Forms.Button();
+            this.Txt_URL = new System.Windows.Forms.TextBox();
+            this.Cmb_VType = new System.Windows.Forms.ComboBox();
+            this.Btn_Record = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Img_video)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Img_video
+            // 
+            this.Img_video.Location = new System.Drawing.Point(106, 128);
+            this.Img_video.Name = "Img_video";
+            this.Img_video.Size = new System.Drawing.Size(588, 299);
+            this.Img_video.TabIndex = 0;
+            this.Img_video.TabStop = false;
+            // 
+            // Btn_Play
+            // 
+            this.Btn_Play.Location = new System.Drawing.Point(632, 12);
+            this.Btn_Play.Name = "Btn_Play";
+            this.Btn_Play.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Play.TabIndex = 2;
+            this.Btn_Play.Text = "재생";
+            this.Btn_Play.UseVisualStyleBackColor = true;
+            this.Btn_Play.Click += new System.EventHandler(this.Btn_Play_Click);
+            // 
+            // Btn_Stop
+            // 
+            this.Btn_Stop.Location = new System.Drawing.Point(713, 12);
+            this.Btn_Stop.Name = "Btn_Stop";
+            this.Btn_Stop.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Stop.TabIndex = 3;
+            this.Btn_Stop.Text = "중지";
+            this.Btn_Stop.UseVisualStyleBackColor = true;
+            this.Btn_Stop.Click += new System.EventHandler(this.Btn_Stop_Click);
+            // 
+            // Txt_URL
+            // 
+            this.Txt_URL.Location = new System.Drawing.Point(504, 12);
+            this.Txt_URL.Name = "Txt_URL";
+            this.Txt_URL.Size = new System.Drawing.Size(100, 21);
+            this.Txt_URL.TabIndex = 4;
+            this.Txt_URL.Text = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
+            // 
+            // Cmb_VType
+            // 
+            this.Cmb_VType.FormattingEnabled = true;
+            this.Cmb_VType.Items.AddRange(new object[] {
+            "RTSP / RTP",
+            "WebCam"});
+            this.Cmb_VType.Location = new System.Drawing.Point(404, 12);
+            this.Cmb_VType.Name = "Cmb_VType";
+            this.Cmb_VType.Size = new System.Drawing.Size(94, 20);
+            this.Cmb_VType.TabIndex = 5;
+            // 
+            // Btn_Record
+            // 
+            this.Btn_Record.Location = new System.Drawing.Point(12, 12);
+            this.Btn_Record.Name = "Btn_Record";
+            this.Btn_Record.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Record.TabIndex = 1;
+            this.Btn_Record.Text = "녹화";
+            this.Btn_Record.UseVisualStyleBackColor = true;
+            this.Btn_Record.Click += new System.EventHandler(this.Btn_Record_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Cmb_VType);
+            this.Controls.Add(this.Txt_URL);
+            this.Controls.Add(this.Btn_Stop);
+            this.Controls.Add(this.Btn_Play);
+            this.Controls.Add(this.Btn_Record);
+            this.Controls.Add(this.Img_video);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.Img_video)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Img_video;
+        private System.Windows.Forms.Button Btn_Play;
+        private System.Windows.Forms.Button Btn_Stop;
+        private System.Windows.Forms.TextBox Txt_URL;
+        private System.Windows.Forms.ComboBox Cmb_VType;
+        private System.Windows.Forms.Button Btn_Record;
+    }
+}
---
+++ FFMPEGTest1/FFMPEGTest1/MainForm.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Threading;
+using FFmpeg.AutoGen;
+using System.Drawing;
+using System.IO;
+using FFMPEGTest1.FFmpeg;
+using FFMPEGTest1.FFmpeg.Decoder;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace FFMPEGTest1
+{
+    public enum VTYPE
+    {
+        RTSP_RTP = 0,
+        CAM
+    }
+    public partial class MainForm : Form
+    {
+        Thread thread;
+        ThreadStart ts;
+        Dispatcher dispatcher = System.Windows.Application.Current.Dispatcher;
+
+        private bool activeThread;      //thread 활성화 유무
+
+        bool isRecord = false;
+
+        EasyFFmpegManager easyFFmpeg;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            VideoStream vs = new VideoStream();
+            //vs.AVFormatTest();
+            //vs.AVFormatTest2();
+            //vs.AVFormatTest3();
+            vs.AVFormatTest4();
+
+            Application.Exit();
+            FFmpegBinariesHelper.RegisterFFmpegBinaries();
+            easyFFmpeg = new EasyFFmpegManager();
+        }
+
+        private void Btn_Record_Click(object sender, EventArgs e)
+        {
+            if (!isRecord)
+            {
+                string fileName = DateTime.Now.ToString("yyMMdd_hh.mm.ss") + ".mp4";
+                easyFFmpeg.RecordVideo(fileName);
+
+                isRecord = true;
+            }
+            else
+            {
+                easyFFmpeg.StopRecord();
+                isRecord = false;
+            }
+        }
+
+        private void Btn_Play_Click(object sender, EventArgs e)
+        {
+            ////thread 시작 
+            //if (thread.ThreadState == ThreadState.Unstarted)
+            //{
+            //    thread.Start();
+            //}
+            string url = Txt_URL.Text;
+            int type = Cmb_VType.SelectedIndex;
+            if (type == 1)
+            {
+                //비디오 프레임 디코딩 thread 생성
+                ts = new ThreadStart(DecodeAllFramesToImages);
+                thread = new Thread(ts);
+                activeThread = true;
+                if (thread.ThreadState == ThreadState.Unstarted)
+                {
+                    thread.Start();
+                }
+            }
+            else
+            {
+                easyFFmpeg.InitializeFFmpeg(url, (VIDEO_INPUT_TYPE)type);
+
+                easyFFmpeg.PlayVideo();
+                easyFFmpeg.VideoFrameReceived += VideoFrameReceived;
+            }
+        }
+
+        private void Btn_Stop_Click(object sender, EventArgs e)
+        {
+            if (Cmb_VType.SelectedIndex == 1 && thread.IsAlive)
+            {
+                activeThread = false;
+                thread.Join();
+            }
+
+            isRecord = true;
+            Btn_Record_Click(null, null);
+            easyFFmpeg.DisposeFFmpeg();
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (Cmb_VType.SelectedIndex == 1 && thread.IsAlive)
+            {
+                activeThread = false;
+                thread.Join();
+            }
+            easyFFmpeg.DisposeFFmpeg();
+        }
+
+        private void VideoFrameReceived(BitmapImage frame)
+        {
+            dispatcher.BeginInvoke((Action)(() =>
+            {
+                Img_video.Image = BitmapImage2Bitmap(frame);
+            }));
+        }
+
+        #region WebCam Method
+        private unsafe void DecodeAllFramesToImages()
+        {
+            //video="웹캠 디바이스 이름"
+            string device = "video=ABKO APC930 QHD WEBCAM";
+            using (var vsd = new VideoStreamDecoderCam(device))
+            {
+                //Console.WriteLine($"codec name: {vsd.CodecName}");
+
+                var info = vsd.GetContextInfo();
+                info.ToList().ForEach(x => Console.WriteLine($"{x.Key} = {x.Value}"));
+
+                var sourceSize = vsd.FrameSize;
+                var sourcePixelFormat = vsd.PixelFormat;
+                var destinationSize = sourceSize;
+                var destinationPixelFormat = AVPixelFormat.AV_PIX_FMT_BGR24;
+                using (var vfc = new VideoFrameConverterCam(sourceSize, sourcePixelFormat, destinationSize, destinationPixelFormat))
+                {
+                    var frameNumber = 0;
+                    while (vsd.TryDecodeNextFrame(out var frame) && activeThread)
+                    {
+                        var convertedFrame = vfc.Convert(frame);
+
+                        Bitmap bitmap;
+
+                        bitmap = new Bitmap(convertedFrame.width, convertedFrame.height, convertedFrame.linesize[0], System.Drawing.Imaging.PixelFormat.Format24bppRgb, (IntPtr)convertedFrame.data[0]);
+                        BitmapToImageSource(bitmap);
+
+                        frameNumber++;
+                    }
+                }
+            }
+        }
+        void BitmapToImageSource(Bitmap bitmap)
+        {
+            //UI thread에 접근하기 위해 dispatcher 사용
+            dispatcher.BeginInvoke((Action)(() =>
+            {
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    if (thread.IsAlive)
+                    {
+                        bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                        memory.Position = 0;
+                        BitmapImage bitmapimage = new BitmapImage();
+                        bitmapimage.BeginInit();
+                        bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                        //bitmapimage.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                        bitmapimage.StreamSource = memory;
+                        bitmapimage.EndInit();
+
+                        Img_video.Image = BitmapImage2Bitmap(bitmapimage);     //image 컨트롤에 웹캠 이미지 표시
+                    }
+                }
+            }));
+
+        }
+
+        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            // BitmapImage bitmapImage = new BitmapImage(new Uri("../Images/test.png", UriKind.Relative));
+
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+        #endregion
+    }
+}
