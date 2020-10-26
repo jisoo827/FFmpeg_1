2da5d68a14a779a2e2d9dfa42f144556eee4aa9f
jisoo827 <73168875+jisoo827@users.noreply.github.com>
2020-10-26 17:58:53 +0900

    Update MainForm.cs

--- FFMPEGTest1/FFMPEGTest1/MainForm.cs
+++ FFMPEGTest1/FFMPEGTest1/MainForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -10,19 +10,28 @@ using FFMPEGTest1.FFmpeg;
 using FFMPEGTest1.FFmpeg.Decoder;
 using System.Windows.Forms;
 using System.Windows.Media.Imaging;
+using System.Diagnostics;
+using System.ComponentModel;
+
 
 namespace FFMPEGTest1
 {
     public enum VTYPE
     {
         RTSP_RTP = 0,
-        CAM
+        CAM = 1,
+        MP4 = 2
     }
     public partial class MainForm : Form
     {
+        [System.Runtime.InteropServices.DllImport("User32.dll", EntryPoint = "PostMessageA")]
+        private static extern bool PostMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
+
+        int Key_Q = 81;
         Thread thread;
         ThreadStart ts;
         Dispatcher dispatcher = System.Windows.Application.Current.Dispatcher;
+        string _FFMPEGPath = @"C:\Users\jisu827\Downloads\ffmpeg-4.3.1-win64-static\ffmpeg-4.3.1-win64-static\bin\ffmpeg.exe";
 
         private bool activeThread;      //thread 활성화 유무
 
@@ -37,30 +46,51 @@ namespace FFMPEGTest1
             //vs.AVFormatTest();
             //vs.AVFormatTest2();
             //vs.AVFormatTest3();
-            vs.AVFormatTest4();
+            //vs.AVFormatTest4();
 
-            Application.Exit();
+            //Application.Exit();
             FFmpegBinariesHelper.RegisterFFmpegBinaries();
             easyFFmpeg = new EasyFFmpegManager();
+            Wmp_1.uiMode = "none";
+            Wmp_1.URL = Txt_URL.Text;
+            Wmp_1.Ctlcontrols.stop();
+
         }
 
         private void Btn_Record_Click(object sender, EventArgs e)
         {
-            if (!isRecord)
-            {
-                string fileName = DateTime.Now.ToString("yyMMdd_hh.mm.ss") + ".mp4";
-                easyFFmpeg.RecordVideo(fileName);
 
-                isRecord = true;
-            }
-            else
-            {
-                easyFFmpeg.StopRecord();
-                isRecord = false;
-            }
+            Point point = this.DesktopLocation;
+            point.X += Wmp_1.Location.X;
+            point.Y += Wmp_1.Location.Y;
+            Process process = new Process();
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.FileName = _FFMPEGPath;
+            //process.StartInfo.Arguments = @"-y -rtbufsize 100M -f gdigrab -framerate 30 -probesize 10M -draw_mouse 1 -i desktop -c:v libx264 -r 30 -preset ultrafast -tune zerolatency -crf 25 -pix_fmt yuv420p ""output.avi""";
+            process.StartInfo.Arguments = @"-f gdigrab -framerate 30 -offset_x " + point.X.ToString() + " -offset_y " + point.Y.ToString() + " -video_size 640x480 -show_region 1 -i desktop output1.avi";
+            
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+            #region old
+            //if (!isRecord)
+            //{
+            //    string fileName = DateTime.Now.ToString("yyMMdd_hh.mm.ss") + ".mp4";
+            //    easyFFmpeg.RecordVideo(fileName);
+
+            //    isRecord = true;
+            //}
+            //else
+            //{
+            //    easyFFmpeg.StopRecord();
+            //    isRecord = false;
+            //} 
+            #endregion
         }
 
-        private void Btn_Play_Click(object sender, EventArgs e)
+        /*private void Btn_Play_Click(object sender, EventArgs e)
         {
             ////thread 시작 
             //if (thread.ThreadState == ThreadState.Unstarted)
@@ -87,9 +117,18 @@ namespace FFMPEGTest1
                 easyFFmpeg.PlayVideo();
                 easyFFmpeg.VideoFrameReceived += VideoFrameReceived;
             }
+        }*/
+
+        private void Btn_Play_Click(object sender, EventArgs e)
+        {
+            VideoStream vs = new VideoStream();
+            int[] video_size = vs.AVFormatTest3(Txt_URL.Text);
+            this.Wmp_1.Size = new System.Drawing.Size(video_size[0], video_size[1]);
+            Wmp_1.URL = Txt_URL.Text;
+            Wmp_1.Ctlcontrols.play();
         }
 
-        private void Btn_Stop_Click(object sender, EventArgs e)
+        /*private void Btn_Stop_Click(object sender, EventArgs e)
         {
             if (Cmb_VType.SelectedIndex == 1 && thread.IsAlive)
             {
@@ -97,19 +136,25 @@ namespace FFMPEGTest1
                 thread.Join();
             }
 
-            isRecord = true;
-            Btn_Record_Click(null, null);
+            if(isRecord) Btn_Record_Click(null, null);
             easyFFmpeg.DisposeFFmpeg();
+        }
+        */
+        private void Btn_Stop_Click(object sender, EventArgs e)
+        {
+            Wmp_1.Ctlcontrols.stop();
         }
 
         private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-            if (Cmb_VType.SelectedIndex == 1 && thread.IsAlive)
-            {
-                activeThread = false;
-                thread.Join();
-            }
-            easyFFmpeg.DisposeFFmpeg();
+            //if (Cmb_VType.SelectedIndex == 1 && thread.IsAlive)
+            //{
+            //    activeThread = false;
+            //    thread.Join();
+            //}
+            //easyFFmpeg.DisposeFFmpeg();
+
+            Application.Exit();
         }
 
         private void VideoFrameReceived(BitmapImage frame)
@@ -194,5 +239,48 @@ namespace FFMPEGTest1
         }
 
         #endregion
+
+        private void btn_RecordStop_Click(object sender, EventArgs e)
+        {
+            string process = "ffmpeg";
+            Process p = Process.GetProcessesByName(process).FirstOrDefault();
+
+            if (p != null)
+            {
+                IntPtr hWnd = p.MainWindowHandle;
+                PostMessage(hWnd, 0x100, Key_Q, 0);
+
+                try
+                {
+                    p.Kill();
+                }
+                catch (InvalidOperationException) { }
+                catch (Win32Exception) { }
+            }
+        }
+
+        private void Btn_Merge_Click(object sender, EventArgs e)
+        {
+            
+            StreamWriter writer;
+            writer = File.CreateText(Application.StartupPath + "\\mergeVideo.txt");        
+            writer.WriteLine("file output2.avi");
+            writer.WriteLine("file output1.avi");
+            writer.Close();
+
+            string filename = Path.GetTempFileName();
+
+            Process process = new Process();
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.FileName = _FFMPEGPath;
+            process.StartInfo.Arguments = @"-f concat -i mergeVideo.txt -c copy output_merge.avi";
+
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+            Txt_URL.Text = Application.StartupPath + "\\output_merge.avi";
+        }
     }
 }
