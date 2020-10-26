c5adc8f9050c558900b2834e9908131d14dd3099
jisoo827 <73168875+jisoo827@users.noreply.github.com>
2020-10-26 17:59:25 +0900

    Update MainForm.Designer.cs

--- FFMPEGTest1/FFMPEGTest1/MainForm.Designer.cs
+++ FFMPEGTest1/FFMPEGTest1/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace FFMPEGTest1
+namespace FFMPEGTest1
 {
     partial class MainForm
     {
@@ -28,22 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.Img_video = new System.Windows.Forms.PictureBox();
             this.Btn_Play = new System.Windows.Forms.Button();
             this.Btn_Stop = new System.Windows.Forms.Button();
             this.Txt_URL = new System.Windows.Forms.TextBox();
             this.Cmb_VType = new System.Windows.Forms.ComboBox();
             this.Btn_Record = new System.Windows.Forms.Button();
+            this.Wmp_1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Btn_RecordStop = new System.Windows.Forms.Button();
+            this.Btn_Merge = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Img_video)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wmp_1)).BeginInit();
             this.SuspendLayout();
             // 
             // Img_video
             // 
-            this.Img_video.Location = new System.Drawing.Point(106, 128);
+            this.Img_video.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Img_video.Location = new System.Drawing.Point(100, 73);
             this.Img_video.Name = "Img_video";
-            this.Img_video.Size = new System.Drawing.Size(588, 299);
+            this.Img_video.Size = new System.Drawing.Size(551, 333);
             this.Img_video.TabIndex = 0;
             this.Img_video.TabStop = false;
+            this.Img_video.Visible = false;
             // 
             // Btn_Play
             // 
@@ -71,18 +78,19 @@
             this.Txt_URL.Name = "Txt_URL";
             this.Txt_URL.Size = new System.Drawing.Size(100, 21);
             this.Txt_URL.TabIndex = 4;
-            this.Txt_URL.Text = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
+            this.Txt_URL.Text = "C:\\_Works\\쏘쓰\\FFmpeg_1-main\\FFMPEGTest1\\FFMPEGTest1\\bin\\Debug\\\\output_merge.avi";
             // 
             // Cmb_VType
             // 
             this.Cmb_VType.FormattingEnabled = true;
             this.Cmb_VType.Items.AddRange(new object[] {
-            "RTSP / RTP",
-            "WebCam"});
+            "WebCam",
+            "MP4"});
             this.Cmb_VType.Location = new System.Drawing.Point(404, 12);
             this.Cmb_VType.Name = "Cmb_VType";
             this.Cmb_VType.Size = new System.Drawing.Size(94, 20);
             this.Cmb_VType.TabIndex = 5;
+            this.Cmb_VType.Text = "WebCam";
             // 
             // Btn_Record
             // 
@@ -94,21 +102,57 @@
             this.Btn_Record.UseVisualStyleBackColor = true;
             this.Btn_Record.Click += new System.EventHandler(this.Btn_Record_Click);
             // 
+            // Wmp_1
+            // 
+            this.Wmp_1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Wmp_1.Enabled = true;
+            this.Wmp_1.Location = new System.Drawing.Point(100, 73);
+            this.Wmp_1.Name = "Wmp_1";
+            this.Wmp_1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Wmp_1.OcxState")));
+            this.Wmp_1.Size = new System.Drawing.Size(551, 333);
+            this.Wmp_1.TabIndex = 6;
+            // 
+            // Btn_RecordStop
+            // 
+            this.Btn_RecordStop.Location = new System.Drawing.Point(93, 12);
+            this.Btn_RecordStop.Name = "Btn_RecordStop";
+            this.Btn_RecordStop.Size = new System.Drawing.Size(75, 23);
+            this.Btn_RecordStop.TabIndex = 1;
+            this.Btn_RecordStop.Text = "녹화중지";
+            this.Btn_RecordStop.UseVisualStyleBackColor = true;
+            this.Btn_RecordStop.Click += new System.EventHandler(this.btn_RecordStop_Click);
+            // 
+            // Btn_Merge
+            // 
+            this.Btn_Merge.Location = new System.Drawing.Point(174, 12);
+            this.Btn_Merge.Name = "Btn_Merge";
+            this.Btn_Merge.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Merge.TabIndex = 1;
+            this.Btn_Merge.Text = "합치기";
+            this.Btn_Merge.UseVisualStyleBackColor = true;
+            this.Btn_Merge.Click += new System.EventHandler(this.Btn_Merge_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Wmp_1);
             this.Controls.Add(this.Cmb_VType);
             this.Controls.Add(this.Txt_URL);
             this.Controls.Add(this.Btn_Stop);
             this.Controls.Add(this.Btn_Play);
+            this.Controls.Add(this.Btn_Merge);
+            this.Controls.Add(this.Btn_RecordStop);
             this.Controls.Add(this.Btn_Record);
             this.Controls.Add(this.Img_video);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.Img_video)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wmp_1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,5 +166,8 @@
         private System.Windows.Forms.TextBox Txt_URL;
         private System.Windows.Forms.ComboBox Cmb_VType;
         private System.Windows.Forms.Button Btn_Record;
+        private AxWMPLib.AxWindowsMediaPlayer Wmp_1;
+        private System.Windows.Forms.Button Btn_RecordStop;
+        private System.Windows.Forms.Button Btn_Merge;
     }
 }
