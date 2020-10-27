f2b61e3b48c173a5750b26297a40c28dc4715b5d
Kim Ji Soo <holy827@gmail.com>
2020-10-28 05:40:10 +0900

    jjcast

---
+++ JJCastDemo/JJCastDemo/FFmpeg/VideoStream.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Diagnostics;
+using FFmpeg.AutoGen;
+
+namespace JJCastDemo.FFmpeg
+{
+    public unsafe class VideoStream : IDisposable
+    {
+        AVFormatContext* _fmtCtx;
+        int _vidx = -1, _aidx = -1;
+        AVStream* _vStream, _aStream;
+        AVCodecParameters* _vPara, _aPara;
+        AVCodec* _vCodec, _aCodec;
+        AVCodecContext* _vCtx, _aCtx;
+        AVPacket _packet;
+        AVFrame _vFrame, _aFrame;
+
+        /// <summary>
+        /// 스트림 찾기
+        /// </summary>
+        /// <returns></returns>
+        public int AVFormatTest()
+        {
+            var fmtCtx = _fmtCtx;
+            int ret = ffmpeg.avformat_open_input(&fmtCtx, "E:\\_Works\\All of Me (Jon Schmidt) - The Piano Guys.mp4", null, null);
+            if (ret != 0) return -1;
+            ffmpeg.avformat_find_stream_info(fmtCtx, null);
+
+            Debug.WriteLine("스트림 개수 = {0:d}\n", fmtCtx->nb_streams);
+
+            Debug.WriteLine("시간 = {0:d}초\n", fmtCtx->duration / ffmpeg.AV_TIME_BASE);
+
+            Debug.WriteLine("비트레이트 = {0:d}\n", fmtCtx->bit_rate);
+            ffmpeg.avformat_close_input(&fmtCtx);
+            return 0;
+        }
+
+        /// <summary>
+        /// 스트림 정보
+        /// </summary>
+        /// <returns></returns>
+        public int AVFormatTest2()
+        {
+            var fmtCtx = _fmtCtx;
+            int ret = ffmpeg.avformat_open_input(&fmtCtx, "E:\\_Works\\All of Me (Jon Schmidt) - The Piano Guys.mp4", null, null);
+            if (ret != 0) return -1;
+            ffmpeg.avformat_find_stream_info(fmtCtx, null);
+
+            for (int i = 0; i < fmtCtx->nb_streams; i++)
+            {
+                if (fmtCtx->streams[i]->codecpar->codec_type == AVMediaType.AVMEDIA_TYPE_VIDEO) _vidx = i;
+                if (fmtCtx->streams[i]->codecpar->codec_type == AVMediaType.AVMEDIA_TYPE_AUDIO) _aidx = i;
+            }
+
+            Debug.WriteLine("video = " + _vidx.ToString() + "번, audio = " + _aidx.ToString() + "번\n");
+
+            _vidx = ffmpeg.av_find_best_stream(fmtCtx, AVMediaType.AVMEDIA_TYPE_VIDEO, -1, -1, null, 0);
+            _aidx = ffmpeg.av_find_best_stream(fmtCtx, AVMediaType.AVMEDIA_TYPE_AUDIO, -1, _vidx, null, 0);
+
+            Debug.WriteLine("video = " + _vidx.ToString() + "번, audio = " + _aidx.ToString() + "번\n");
+
+            ffmpeg.avformat_close_input(&fmtCtx);
+            return 0;
+        }
+
+        /// <summary>
+        /// 코덱 정보 조사
+        /// </summary>
+        /// <returns></returns>
+        public int[] AVFormatTest3(string url)
+        {
+            int[] rtnval = new int[2];
+            var fmtCtx = _fmtCtx;
+            int ret = ffmpeg.avformat_open_input(&fmtCtx, url, null, null);
+            if (ret != 0) return rtnval;
+            ffmpeg.avformat_find_stream_info(fmtCtx, null);
+
+            _vidx = ffmpeg.av_find_best_stream(fmtCtx, AVMediaType.AVMEDIA_TYPE_VIDEO, -1, -1, null, 0);
+            _aidx = ffmpeg.av_find_best_stream(fmtCtx, AVMediaType.AVMEDIA_TYPE_AUDIO, -1, _vidx, null, 0);
+
+            _vStream = fmtCtx->streams[_vidx];
+            Debug.WriteLine("프레임 개수 = " + _vStream->nb_frames);
+            Debug.WriteLine("프레임 레이트 = " + _vStream->avg_frame_rate.num + " / " + _vStream->avg_frame_rate.den);
+            Debug.WriteLine("타임 베이스 = " + _vStream->time_base.num + " / " + _vStream->time_base.den);
+            _vPara = _vStream->codecpar;
+            Debug.WriteLine("폭 = " + _vPara->width);
+            Debug.WriteLine("높이 = " + _vPara->height);
+            Debug.WriteLine("색상 포맷 = " + _vPara->format);
+            Debug.WriteLine("코덱 = " + _vPara->codec_id);
+            Debug.WriteLine("--------------------------------");
+            rtnval[0] = _vPara->width;
+            rtnval[1] = _vPara->height;
+            return rtnval;
+            /*_aStream = fmtCtx->streams[_aidx];
+            Debug.WriteLine("프레임 개수 = " + _aStream->nb_frames);
+            Debug.WriteLine("타임 베이스 = " + _aStream->time_base.num + " / " + _aStream->time_base.den);
+            _aPara = _aStream->codecpar;
+            Debug.WriteLine("사운드 포맷 = " + _aPara->format);
+            Debug.WriteLine("코덱 = " + _aPara->codec_id);
+            Debug.WriteLine("채널 = " + _aPara->channels);
+            Debug.WriteLine("샘플 레이트 = " + _aPara->sample_rate);
+
+            ffmpeg.avformat_close_input(&fmtCtx);
+            return 0;*/
+        }
+
+        /// <summary>
+        /// 압축 해제
+        /// </summary>
+        /// <returns></returns>
+        public int AVFormatTest4()
+        {
+            var fmtCtx = _fmtCtx;
+            int ret = ffmpeg.avformat_open_input(&fmtCtx, "C:\\Users\\jisu827\\output.avi", null, null);
+            if (ret != 0) return -1;
+            ffmpeg.avformat_find_stream_info(fmtCtx, null);
+
+            _vidx = ffmpeg.av_find_best_stream(fmtCtx, AVMediaType.AVMEDIA_TYPE_VIDEO, -1, -1, null, 0);
+            _aidx = ffmpeg.av_find_best_stream(fmtCtx, AVMediaType.AVMEDIA_TYPE_AUDIO, -1, _vidx, null, 0);
+
+            var packet = _packet;
+
+            var vFrame = _vFrame;
+            var vCtx = _vCtx;
+            _vStream = fmtCtx->streams[_vidx];
+            _vPara = _vStream->codecpar;
+            _vCodec = ffmpeg.avcodec_find_decoder(_vPara->codec_id);
+            vCtx = ffmpeg.avcodec_alloc_context3(_vCodec);
+            ffmpeg.avcodec_parameters_to_context(vCtx, _vPara);
+            ffmpeg.avcodec_open2(vCtx, _vCodec, null);
+
+            var aFrame = _aFrame;
+            var aCtx = _aCtx;
+            _aStream = fmtCtx->streams[_aidx];
+            _aPara = _aStream->codecpar;
+            _aCodec = ffmpeg.avcodec_find_decoder(_aPara->codec_id);
+            aCtx = ffmpeg.avcodec_alloc_context3(_aCodec);
+            ffmpeg.avcodec_parameters_to_context(aCtx, _aPara);
+            ffmpeg.avcodec_open2(aCtx, _aCodec, null);
+
+            // 루프를 돌며 패킷을 모두 읽는다.
+            int vcount = 0, acount = 0;
+            int cnt = 0;
+            while (ffmpeg.av_read_frame(fmtCtx, &packet) == 0)
+            {
+                if (packet.stream_index == _vidx)
+                {
+                    ret = ffmpeg.avcodec_send_packet(vCtx, &packet);
+                    if (ret != 0) { continue; }
+                    for (; ; )
+                    {
+                        ret = ffmpeg.avcodec_receive_frame(vCtx, &vFrame);
+                        if (ret == ffmpeg.AVERROR(ffmpeg.EAGAIN)) break;
+                        if (vcount == 0)
+                        {
+                            Debug.WriteLine(string.Format("Video format : {0}({1} x {2}).",
+                                vFrame.format, vFrame.width, vFrame.height));
+                        }
+                        Debug.Write(string.Format("V{0,5}(pts={1,5},size={2,5}) : ", vcount++, vFrame.pts, vFrame.pkt_size));
+                        for (uint i = 0; i < 3; i++)
+                        {
+                            Debug.Write(vFrame.linesize[i] + " ");
+                        }
+                        arDump(vFrame.data[0], 4);
+                        arDump(vFrame.data[1], 2);
+                        arDump(vFrame.data[2], 2);
+
+                    }
+
+                }
+
+                if (packet.stream_index == _aidx)
+                {
+                    ret = ffmpeg.avcodec_send_packet(aCtx, &packet);
+                    if (ret != 0) { continue; }
+                    for (; ; )
+                    {
+                        ret = ffmpeg.avcodec_receive_frame(aCtx, &aFrame);
+                        if (ret == ffmpeg.AVERROR(ffmpeg.EAGAIN)) break;
+                        if (acount == 0)
+                        {
+                            Debug.WriteLine(string.Format("Audio format : {0}({1} x {2}).",
+                                aFrame.format, aFrame.channels, aFrame.sample_rate));
+                        }
+                        Debug.Write(string.Format("A{0,5}(pts={1,5},size={2,5}) : ", acount++, aFrame.pts, aFrame.pkt_size));
+                        arDump(aFrame.extended_data, 16);
+                    }
+                }
+
+                ffmpeg.av_packet_unref(&packet);
+                Debug.WriteLine("");
+                if (cnt == 1000) break;
+                cnt++;
+            }
+
+
+            // 메모리 해제
+            ffmpeg.av_frame_unref(&vFrame);
+            ffmpeg.av_frame_unref(&aFrame);
+            ffmpeg.avcodec_free_context(&vCtx);
+            ffmpeg.avcodec_free_context(&aCtx);
+            ffmpeg.avformat_close_input(&fmtCtx);
+            return 0;
+        }
+
+        void arDump(void* array, int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Debug.Write(string.Format("{0,3:X2}", ((byte*)array + i) == null ? 0 : *((byte*)array + i)));
+            }
+        }
+
+        public void Dispose()
+        {
+            var _iFormatContext = _fmtCtx;
+            ffmpeg.avformat_close_input(&_iFormatContext);
+        }
+    }
+}
---
+++ JJCastDemo/JJCastDemo/MainForm.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace JJCastDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Img_video = new System.Windows.Forms.PictureBox();
+            this.Btn_Play = new System.Windows.Forms.Button();
+            this.Btn_Stop = new System.Windows.Forms.Button();
+            this.Txt_URL = new System.Windows.Forms.TextBox();
+            this.Cmb_VType = new System.Windows.Forms.ComboBox();
+            this.Btn_Record = new System.Windows.Forms.Button();
+            this.Wmp_1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Btn_RecordStop = new System.Windows.Forms.Button();
+            this.Btn_Merge = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Img_video)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wmp_1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Img_video
+            // 
+            this.Img_video.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.Img_video.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Img_video.Location = new System.Drawing.Point(464, 65);
+            this.Img_video.Name = "Img_video";
+            this.Img_video.Size = new System.Drawing.Size(243, 165);
+            this.Img_video.TabIndex = 0;
+            this.Img_video.TabStop = false;
+            this.Img_video.Visible = false;
+            // 
+            // Btn_Play
+            // 
+            this.Btn_Play.Location = new System.Drawing.Point(295, 12);
+            this.Btn_Play.Name = "Btn_Play";
+            this.Btn_Play.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Play.TabIndex = 2;
+            this.Btn_Play.Text = "재생";
+            this.Btn_Play.UseVisualStyleBackColor = true;
+            this.Btn_Play.Click += new System.EventHandler(this.Btn_Play_Click);
+            // 
+            // Btn_Stop
+            // 
+            this.Btn_Stop.Location = new System.Drawing.Point(376, 12);
+            this.Btn_Stop.Name = "Btn_Stop";
+            this.Btn_Stop.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Stop.TabIndex = 3;
+            this.Btn_Stop.Text = "중지";
+            this.Btn_Stop.UseVisualStyleBackColor = true;
+            this.Btn_Stop.Click += new System.EventHandler(this.Btn_Stop_Click);
+            // 
+            // Txt_URL
+            // 
+            this.Txt_URL.Location = new System.Drawing.Point(12, 130);
+            this.Txt_URL.Name = "Txt_URL";
+            this.Txt_URL.Size = new System.Drawing.Size(613, 21);
+            this.Txt_URL.TabIndex = 4;
+            this.Txt_URL.Text = "C:\\_Works\\쏘쓰\\FFmpeg_1-main\\FFMPEGTest1\\FFMPEGTest1\\bin\\Debug\\\\output_merge.avi";
+            // 
+            // Cmb_VType
+            // 
+            this.Cmb_VType.FormattingEnabled = true;
+            this.Cmb_VType.Items.AddRange(new object[] {
+            "WebCam",
+            "Video"});
+            this.Cmb_VType.Location = new System.Drawing.Point(74, 24);
+            this.Cmb_VType.Name = "Cmb_VType";
+            this.Cmb_VType.Size = new System.Drawing.Size(185, 20);
+            this.Cmb_VType.TabIndex = 5;
+            this.Cmb_VType.Text = "WebCam";
+            // 
+            // Btn_Record
+            // 
+            this.Btn_Record.Location = new System.Drawing.Point(295, 66);
+            this.Btn_Record.Name = "Btn_Record";
+            this.Btn_Record.Size = new System.Drawing.Size(106, 23);
+            this.Btn_Record.TabIndex = 1;
+            this.Btn_Record.Text = "녹화";
+            this.Btn_Record.UseVisualStyleBackColor = true;
+            this.Btn_Record.Click += new System.EventHandler(this.Btn_Record_Click);
+            // 
+            // Wmp_1
+            // 
+            this.Wmp_1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Wmp_1.Enabled = true;
+            this.Wmp_1.Location = new System.Drawing.Point(3, 17);
+            this.Wmp_1.Name = "Wmp_1";
+            this.Wmp_1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Wmp_1.OcxState")));
+            this.Wmp_1.Size = new System.Drawing.Size(452, 262);
+            this.Wmp_1.TabIndex = 6;
+            // 
+            // Btn_RecordStop
+            // 
+            this.Btn_RecordStop.Location = new System.Drawing.Point(407, 66);
+            this.Btn_RecordStop.Name = "Btn_RecordStop";
+            this.Btn_RecordStop.Size = new System.Drawing.Size(106, 23);
+            this.Btn_RecordStop.TabIndex = 1;
+            this.Btn_RecordStop.Text = "녹화중지";
+            this.Btn_RecordStop.UseVisualStyleBackColor = true;
+            this.Btn_RecordStop.Click += new System.EventHandler(this.btn_RecordStop_Click);
+            // 
+            // Btn_Merge
+            // 
+            this.Btn_Merge.Location = new System.Drawing.Point(295, 95);
+            this.Btn_Merge.Name = "Btn_Merge";
+            this.Btn_Merge.Size = new System.Drawing.Size(106, 23);
+            this.Btn_Merge.TabIndex = 1;
+            this.Btn_Merge.Text = "합성";
+            this.Btn_Merge.UseVisualStyleBackColor = true;
+            this.Btn_Merge.Click += new System.EventHandler(this.Btn_Merge_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.Wmp_1);
+            this.groupBox1.Controls.Add(this.Img_video);
+            this.groupBox1.Location = new System.Drawing.Point(12, 157);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(710, 284);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(407, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "합성 영상 재생";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Btn_Merge_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(519, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 46);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "크로마키 칼라\r\nRGB (0, 0, 0)";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.Cmb_VType);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(277, 112);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "설정";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Mic";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Cam";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "WebCam",
+            "Video"});
+            this.comboBox1.Location = new System.Drawing.Point(74, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(185, 20);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "WebCam";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Monitor";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "WebCam",
+            "Video"});
+            this.comboBox2.Location = new System.Drawing.Point(74, 76);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(185, 20);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.Text = "WebCam";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(330, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 21);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(295, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "제목";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 444);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Txt_URL);
+            this.Controls.Add(this.Btn_Stop);
+            this.Controls.Add(this.Btn_Play);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Btn_Merge);
+            this.Controls.Add(this.Btn_RecordStop);
+            this.Controls.Add(this.Btn_Record);
+            this.Name = "MainForm";
+            this.Text = "JJCastDemo";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Img_video)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wmp_1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Img_video;
+        private System.Windows.Forms.Button Btn_Play;
+        private System.Windows.Forms.Button Btn_Stop;
+        private System.Windows.Forms.TextBox Txt_URL;
+        private System.Windows.Forms.ComboBox Cmb_VType;
+        private System.Windows.Forms.Button Btn_Record;
+        private AxWMPLib.AxWindowsMediaPlayer Wmp_1;
+        private System.Windows.Forms.Button Btn_RecordStop;
+        private System.Windows.Forms.Button Btn_Merge;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
